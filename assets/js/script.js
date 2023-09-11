c2ea4c029dc0dfc90a01e1a9fae8d589102bd0ef
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-11 14:57:57 +0200

    [ADD]: const boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,18 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipl
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 1. Creare un ciclo for da 1 a 100;
+
 1a. ad ogni multiplo di 3 stampare la stringa "Fizz";
 2a. ad ogni multiplo di 5 stampare la stringa "Buzz";
-3a. se multiplo sia di 3, sia di 5 stampare "FizzBuzz"
+3a. se multiplo sia di 3, sia di 5 stampare "FizzBuzz".
 
+
+2. dichiaro la variabile che contiene il percorso del div 'boxes';
 
 */
 
-// 1.
+const boxes = document.querySelector('.boxes');
+
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     console.log("Fizz");
