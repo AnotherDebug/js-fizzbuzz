d05bfcbbf0518c353cae9beccbd324923fa1aa2f
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-11 13:37:58 +0200

    [IPDATE]: ciclo for

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,18 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipl
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 1. Creare un ciclo for da 1 a 100;
+2. ad ogni multiplo di 3 stampare la stringa "Fizz";
 
 */
 
 // 1.
-for(let i = 1; i <= 100; i++){
-    console.log(i);
+for(let i = 1; i <= 100; i++) {
+    
+    if(i % 3 === 0) {
+        console.log('Fizz');
+    }else {
+        console.log(i);
+    };
 };
+
+ 
